4fe5f601e04aa5d3b7351f7e34dc45e4d6db65c9
Dmitry Pevnitsky <pandarec13@yandex.ru>
2021-01-30 01:16:20 +0300

    finished

--- app/src/main/java/com/example/firebasestart2/screens/addition/AddPersonActivity.java
+++ app/src/main/java/com/example/firebasestart2/screens/addition/AddPersonActivity.java
@@ -2,11 +2,15 @@ package com.example.firebasestart2.screens.addition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.firebasestart2.R;
+import com.example.firebasestart2.pojo.Person;
+import com.example.firebasestart2.screens.start.MainActivity;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AddPersonActivity extends AppCompatActivity {
@@ -30,6 +34,25 @@ public class AddPersonActivity extends AppCompatActivity {
     }
 
     public void onClickAddButtonPressed(View view) {
+        if(!isCorrect()){
+            Toast.makeText(this, "Заполните все поля", Toast.LENGTH_SHORT).show();
+            return;
+        }
+            Person person = new Person();
+            person.setName(editTextName.getText().toString());
+            person.setLastName(editTextLastName.getText().toString());
+            int age;
+            try {
+                 age = Integer.parseInt(editTextAge.getText().toString());
+            }catch (NumberFormatException e){
+                Toast.makeText(this, "Введите возраст целым числом", Toast.LENGTH_SHORT).show();
+                editTextAge.getText().clear();
+                return;
+            }
+            person.setAge(age);
+            database.collection("people").add(person);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
 
 
     }
